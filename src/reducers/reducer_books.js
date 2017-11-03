7fb028e7ee8a7b0643d46f74b32cecd37f0a5185
Daniel Mai <danielmai1728@gmail.com>
2017-11-02 22:27:24 -0700

    update active action and reducer

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts'},
-    { title: 'Harry Potter'},
-    { title: 'The Dark Tower'},
-    { title: 'Eloquent Rubyer'}
+    { title: 'Javascript: The Good Parts', pages: 102},
+    { title: 'Harry Potter', pages: 90 },
+    { title: 'The Dark Tower', pages: 200},
+    { title: 'Eloquent Ruby', pages: 23}
   ]
 }
